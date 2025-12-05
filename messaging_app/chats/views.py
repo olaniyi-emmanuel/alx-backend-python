a80c8a7a3e2c54f91ef2001d71bafb63136f9a13
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-05 21:02:10 +0100

    Message viewset and conversationviewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Message, Conversation
+from .serializers import ConversationSerializer, MessageSerializer
 # Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ViewSet):
+    serializer_class = MessageSerializer
+
