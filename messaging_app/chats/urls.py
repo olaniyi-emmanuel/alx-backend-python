3e36cc18010e4cb0009fba686ddff3339d63c283
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-05 22:48:58 +0100

    urls.py added to the chat app

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,10 @@
+from  django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
+
+urlpatterns = ['/conversations', include(router.urls)]
