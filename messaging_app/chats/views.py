1b7b506383837f7556e8b36c0d3626b8966e7a9a
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-12-05 22:22:28 +0100

    Message viewset and conversationviewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Message, Conversation
 from .serializers import ConversationSerializer, MessageSerializer
 # Create your views here.
 
 class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
 
 class MessageViewSet(viewsets.ViewSet):
+    queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+
 
